1700fa174200abc7cc27d4a74509215800516395
dongb909 <dongb909@gmail.com>
2020-07-21 16:29:37 -0700

    Set up game winning color change and reset changes for text and h1

--- color.js
+++ color.js
@@ -11,26 +11,43 @@ let getRandomeColors = (num) => {
 
 let colors = getRandomeColors(6);
 let squares = document.querySelectorAll(".square");
-let pickedColor = colors[Math.floor(Math.random() * colors.length)];
-document.getElementById("targetColor").textContent = pickedColor;
-
+let displayMessage = document.getElementById('displayMessage');
+let resetButton = document.querySelector('#setup')
+let h1 = document.querySelector('h1')
 
 
 let applyColorsToSquares = function (colors) {
+  pickedColor = colors[Math.floor(Math.random() * colors.length)];
+  document.getElementById("targetColor").textContent = pickedColor;
   for(let i = 0; i < squares.length; i++) {
     //add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
-    //add event listeners to squares
+    //add event listeners to squares to compare to targetColor
     squares[i].addEventListener("click", (e) => {
-      //get color and compare with target color
-      if (e.target.style.backgroundColor !== pickedColor){
-        alert('wrong')
+      if (e.target.style.backgroundColor === pickedColor){
+        displayMessage.textContent = "Correcto!!";
+        applyWinningColor(pickedColor);
+        h1.style.backgroundColor = pickedColor;
+        resetButton.textContent = "Play Again?";
       } else {
-        alert('correct')
+        e.target.style.backgroundColor = "#232323";
+        displayMessage.textContent = "Alomost! Try Again";
       }
     })
-
   }
 }
 
 applyColorsToSquares(colors);
+function applyWinningColor(color) {
+  squares.forEach(el => {
+    el.style.backgroundColor = color;
+  }) 
+}
+
+resetButton.addEventListener("click", e => {
+  colors = getRandomeColors(6)
+  applyColorsToSquares(colors);
+  e.target.textContent = "New Colors";
+  displayMessage.textContent = "Alright! Let's continue!";
+  h1.style.backgroundColor = "steelblue"
+})
